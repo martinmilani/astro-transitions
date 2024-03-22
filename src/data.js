45903dfb9aa65aa8731f262f5606700fb8fb3db5
Personal <martin.milani.mdq@gmail.com>
2024-03-22 12:57:59 -0300

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const DATA = [
+  {
+  id: 1,
+  title: 'Air Jordan 1 Retro High OG',
+  subtitle: 'Zapatillas Jordan Unisex',
+  price: 349.999,
+  description: 'El Air Jordan 1 Retro High renueva el calzado clásico, y te ofrece un nuevo look con una sensación familiar. Los materiales premium con nuevos colores y texturas aportan un toque moderno al favorito de siempre.',
+  image: '/images/jordan.png'
+},
+  {
+  id: 2,
+  title: 'Nike Pegasus 39',
+  subtitle: 'Zapatillas de Running para Hombre',
+  price: 209.999,
+  description: 'Deja que el Nike Pegasus 39 te ayude a alcanzar nuevas alturas. Más ligero que el Pegasus 38 en la parte superior e ideal para usar todo el año, tiene una sensación de soporte para mantener los pies seguros; la amortiguación en la planta del pie y las unidades dobles Zoom Air (una más que el Peg 38) les dan un brillo adicional a tus pisadas.',
+  image: '/images/pegasus.png'
+},
+  {
+  id: 3,
+  title: 'Nike Tiempo Legend 10 Pro',
+  subtitle: 'Botines de Pasto Sintético Unisex',
+  price: 254.999,
+  description: 'Incluso las leyendas encuentran la manera de evolucionar. Creado para quienes se toman en serio su deporte, la nueva versión de este calzado Pro tiene el nuevo cuero con diseño avanzado FlyTouch Pro. Más suave que el cuero natural, se adapta a tu pie y funciona con All Conditions Control (una textura adherente incluso en climas húmedos), para que puedas marcar el ritmo de tu partido. Más ligero y elegante que cualquier otro Tiempo hasta la fecha, el Legend 10 es para cualquier posición en el campo, ya sea que estés enviando un pase milimétrico a través de la defensa o mientras regresas para detener una escapada.',
+  image: '/images/tempo.png'
+},
+  {
+  id: 4,
+  title: 'Nike Air Zoom Vomero 16',
+  subtitle: 'Zapatillas de Running para Hombre',
+  price: 269.999,
+  description: 'El Vomero 16 agrega una amortiguación de espuma ZoomX de respuesta inmediata que le da un toque de energía a tus pasos, perfecto para altos kilometrajes en la carretera. Es superligero y cuenta con mucha elasticidad y ventilación en la parte superior. ¿Buscas el soporte adecuado? Con una banda regulable en el mediopié y un clip estable en el talón, tendrás el soporte que necesitas.',
+  image: '/images/vomero.png'
+},
+]
+
+export default DATA
